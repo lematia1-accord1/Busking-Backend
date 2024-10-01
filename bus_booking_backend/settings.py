3b5ed99f2664d8f269a7762abff288beccc6d9c0
Lematia Accord <lematia1accord1@gmail.com>
2024-10-01 19:59:45 +0300

    update database

--- bus_booking_backend/settings.py
+++ bus_booking_backend/settings.py
@@ -18,7 +18,7 @@ import logging
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = False
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
